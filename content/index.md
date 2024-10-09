6c683ef17d697594992c3679b3537ce6cd3d878e
Carlos Eduardo <cedut.dev@gmail.com>
2024-10-09 00:37:44 +0000

    Quartz sync: Oct 9, 2024, 12:37 AM

---
+++ content/index.md
@@ -0,0 +1,16 @@
+---
+title: 👋 Bem-vindo ao Dev Journal!
+description: Um espaço pessoal dedicado ao desenvolvimento e a tecnologia, onde compartilho tutoriais, reflexões e projetos.
+draft: false
+date: 2024-10-08
+---
+
+Olá! Meu nome é **Carlos Eduardo** e este é o meu espaço pessoal dedicado ao desenvolvimento e a tecnologia. Aqui, compartilho minhas experiências, aprendizados e reflexões sobre o mundo do desenvolvimento de software.
+
+## Mídias Sociais
+
+- GitHub: [CarlosEduts](https://github.com/CarlosEduts)
+- Instagram: [@carloseduti](https://www.instagram.com/carloseduti)
+- Portfólio: [www.carlosdev.top](https://www.carlosdev.top)
+
+🪴 Espero que você goste do conteúdo e encontre algo útil aqui!
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/CarlosEduts",
+      Instagram: "https://www.instagram.com/carloseduti",
+      Portfólio: "https://www.carlosdev.top",
     },
   }),
 }
@@ -30,9 +31,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.DesktopOnly(Component.Graph()),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.RecentNotes(),
   ],
 }
 
